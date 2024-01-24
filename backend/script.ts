eb77f4cfd95cab5e071028400ceabf1271d67501
N1NHOS0 <renanpgranzotti@gmail.com>
2024-01-24 20:50:54 -0300

    test

--- backend/script.ts
+++ backend/script.ts
@@ -1,34 +1,32 @@
-import { PrismaClient } from '@prisma/client/edge';
-import express, { Request, Response } from 'express';
-
+import express, { Request, Response } from "express";
+import { prisma } from "./prisma/prisma";
 
 const app = express();
-const prisma = new PrismaClient()
 
-app.use(express.json())
+app.use(express.json());
 app.get("/", async (req: Request, res: Response) => {
-    return res.send("Hello")
-})
+  return res.send("Hello");
+});
 
 app.get("/projects", async (req: Request, res: Response) => {
-    return res.status(200).json(await prisma.projects.count())
-})
+  return res.status(200).json(await prisma.projects.findMany());
+});
 
 app.get("/projects/new", async (req: Request, res: Response) => {
-    await prisma.projects.create({
-        data: {
-            id: "teste-teste",
-            name: "Teste Teste",
-            description: "Descrição teste",
-            github: "github",
-            project: "project"
-        }
-    })
-    return res.send("Criou")
-})
+  await prisma.projects.create({
+    data: {
+      id: "teste-teste",
+      name: "Teste Teste",
+      description: "Descrição teste",
+      github: "github",
+      project: "project",
+    },
+  });
+  return res.send("Criou");
+});
 
 app.listen(8080, () => {
-    console.log("entrou");
-})
+  console.log("entrou");
+});
 
-module.exports = app
+module.exports = app;
