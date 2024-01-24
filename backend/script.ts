062c814f1a854096585fbe12284a7df9cbe59443
N1NHOS0 <renanpgranzotti@gmail.com>
2024-01-23 22:40:40 -0300

    Update

--- backend/script.ts
+++ backend/script.ts
@@ -1,30 +1,30 @@
 import { PrismaClient } from '@prisma/client';
 import express, { Request, Response } from 'express';
 const app = express();
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
 
 app.use(express.json())
 
 app.get("/", async (req: Request, res: Response) => {
-    return res.status(200).send("Hello")
+    return res.send("Hello")
 })
 
-app.get("/projects", async (req: Request, res: Response) => {
-    return res.status(200).json(await prisma.projects.findMany())
-})
+// app.get("/projects", async (req: Request, res: Response) => {
+//     return res.status(200).json(await prisma.projects.findMany())
+// })
 
-app.get("/projects/new", async (req: Request, res: Response) => {
-    await prisma.projects.create({
-        data: {
-            id: "teste-teste",
-            name: "Teste Teste",
-            description: "Descrição teste",
-            github: "github",
-            project: "project"
-        }
-    })
-    return res.send("Criou")
-})
+// app.get("/projects/new", async (req: Request, res: Response) => {
+//     await prisma.projects.create({
+//         data: {
+//             id: "teste-teste",
+//             name: "Teste Teste",
+//             description: "Descrição teste",
+//             github: "github",
+//             project: "project"
+//         }
+//     })
+//     return res.send("Criou")
+// })
 
 app.listen(8080, () => {
     console.log("entrou");
