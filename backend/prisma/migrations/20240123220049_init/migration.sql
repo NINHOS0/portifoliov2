fb7aa5de815160f3ee96d29796b88fe64d9e8812
N1NHOS0 <renanpgranzotti@gmail.com>
2024-01-23 21:59:45 -0300

    Update

--- backend/prisma/migrations/20240123220049_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Projects" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "github" TEXT NOT NULL,
-    "project" TEXT NOT NULL
-);
