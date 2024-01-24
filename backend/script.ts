a2fd946423ca87a6f0bb43695190ab93b0cdcc7d
N1NHOS0 <renanpgranzotti@gmail.com>
2024-01-23 22:25:13 -0300

    Update

--- backend/script.ts
+++ backend/script.ts
@@ -5,8 +5,12 @@ const prisma = new PrismaClient()
 
 app.use(express.json())
 
-app.get("/projects", (req: Request, res: Response) => {
-    return res.status(200).json(prisma.projects.findMany)
+app.get("/", async (req: Request, res: Response) => {
+    return res.status(200).write("Hello")
+})
+
+app.get("/projects", async (req: Request, res: Response) => {
+    return res.status(200).json(await prisma.projects.findMany())
 })
 
 app.listen(8080);
